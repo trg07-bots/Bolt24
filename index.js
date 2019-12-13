d9b224885ac82b663dd9e54f65fecd4073626e8a
trg07-bots <58766724+trg07-bots@users.noreply.github.com>
2019-12-13 11:10:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjU0MjAzMjA1NzY0NTEzNzk0.XfClVw.YoKSpm2yjVaPA7DLrc1A_Nc7iOw';
+const token = 'NjU0MjAzMjA1NzY0NTEzNzk0.XfPh1w.B5PKP2p5_MpL_Vb087EhNhiWd8k';
 
 client.once('ready', () => {
     console.log('Bolt24 is now online')
 })
 
 client.on("message", (message) => {
-/*    if (message.content.startsWith("-kick")) {
-       var member = message.mentions.members.first();
+    if (message.content.startsWith("-kick")) {
+        var member = message.mentions.members.first();
         if (member != null) {
             member.kick().then((member) => {
                 message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-           }).catch(() => {
-                message.channel.send("Access Denied");
+            }).catch((error) => {
+                console.error(error);
             });
         } else {
             message.channel.send('use format \'@username\'');
         }
     }
-*/
     if (message.content === '-marco') {
         message.channel.send('Polo');
     }
